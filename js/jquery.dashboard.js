7af6e6fa725389e4aaa8d2956954e1de9e9c1e0c
mjbertken <michael@bertken.com>
2018-02-10 17:23:58 -0600

    Update jquery.dashboard.js

--- js/jquery.dashboard.js
+++ js/jquery.dashboard.js
@@ -18,20 +18,20 @@
         */
 
         //Line chart
-        Morris.Line({
-            element: 'morris-line-example',
-            data: [
-                { y: '2012', a: 75,  b: 65},
-                { y: '2013', a: 50,  b: 40},
-                { y: '2014', a: 75,  b: 65},
-                { y: '2015', a: 100, b: 90}
-            ],
-            xkey: 'y',
-            ykeys: ['a', 'b'],
-            labels: ['Series A', 'Series B'],
-            resize: true,
-            lineColors: ['#1a2942', '#3bc0c3']
-        });
+//         Morris.Line({
+//             element: 'morris-line-example',
+//             data: [
+//                 { y: '2012', a: 75,  b: 65},
+//                 { y: '2013', a: 50,  b: 40},
+//                 { y: '2014', a: 75,  b: 65},
+//                 { y: '2015', a: 100, b: 90}
+//             ],
+//             xkey: 'y',
+//             ykeys: ['a', 'b'],
+//             labels: ['Series A', 'Series B'],
+//             resize: true,
+//             lineColors: ['#1a2942', '#3bc0c3']
+//         });
 
         //Bar chart
         Morris.Bar({
